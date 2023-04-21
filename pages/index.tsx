8f23b5fedea8a51495e15063d2bc72c270b570fc
Freddy Tomada <freddytomada7@gmail.com>
2023-04-21 15:51:10 -0400

    second part finished

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from 'next';
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
+
+import { Layout } from '../components/layouts';
+import { EntryList, NewEntry } from '../components/ui';
+
+const HomePage: NextPage = () => {
+  return (
+    <Layout title='Home - OpenJira'>
+      
+      <Grid container spacing={ 2 }>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="Pending" />
+
+            {/* Agregar una nueva entrada */}
+            {/* Listado de las entradas */}
+            <NewEntry />
+            <EntryList status='pending'/>
+
+
+          </Card>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="In Progress" />
+            <EntryList status='in-progress' />
+          </Card>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="Finished" />
+            <EntryList status='finished' />
+          </Card>
+        </Grid>
+
+
+      </Grid>
+
+    </Layout>
+  )
+}
+
+export default HomePage;
